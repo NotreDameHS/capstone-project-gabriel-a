d2aa381c7b928f1cd63b979ec699aece430a40e1
Gabriel Martinez <gabetom0324@hotmail.com>
2026-05-29 12:57:41 -0600

    Game_manager now reads WaveData.txt which controls the waves system so that modification of Waves is easier later on. it also adds the ability to add more waves or even add a hard mode with more higher ranked enemies!

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -1,11 +1,53 @@
 extends Node
-
-
+const mob_spawner = preload("res://entities/Mobs/MobSpawner/mob_spawner.tscn")
+var waveData: Array = []
+var maxWaves: int # This will be extracted from the WaveData.txt File!
+var currentWave = 5
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	print("Game manager Started!")
+	load_wave_file("res://WaveData.txt")
+	wave_manager(currentWave) #this is here temporary to test wave manager
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+func load_wave_file(path: String) -> void: #
+	if not FileAccess.file_exists(path):
+		push_error("Wave file missing: " + path)
+		return
+	var file = FileAccess.open(path, FileAccess.READ)
+	while not file.eof_reached():
+		var line = file.get_line().strip_edges()
+		# Skipping empty lines and comments starting with '#'
+		if line == "" or line.begins_with("#"): 
+			continue
+		if line.begins_with("@"):
+			line.remove_chars("@ maximum waves = ")
+			maxWaves = int(line)
+			continue
+		var wave_dict = {}
+		# Split different enemy groups in the same wave (separated by ';')
+		var enemy_groups = line.split(";")
+		for group in enemy_groups:
+			var data = group.split(",") # Split enemy_name and amount
+			if data.size() == 2:
+				var enemy_type = data[0].strip_edges()
+				var amount = int(data[1].strip_edges()) # Cast string to integer
+				wave_dict[enemy_type] = amount
+		waveData.append(wave_dict) #finally adds WaveData to a list.
+	file.close()
+
+func get_wave_instructions(wave_index: int) -> Dictionary:
+	if wave_index >= 0 and wave_index < waveData.size():
+		return waveData[wave_index]
+	return {} # Return empty dictionary if wave doesn't exist
+
+func wave_manager(requestedWavenumber: int) -> void:
+	var requestedWave = get_wave_instructions(requestedWavenumber)
+	print(requestedWave)
+	
+	
--- entities/Mobs/MobSpawner/mob_spawner.gd
+++ entities/Mobs/MobSpawner/mob_spawner.gd
@@ -1,26 +1,35 @@
 extends Node2D
 @onready var timer = $Timer
 @export var mob_types: Array[PackedScene] 
-@export var mobsToSpawn: Array #Will receive the mobs that will spawn this round.
 @export var spawnRate: float
 @export var mobsSpawned =  0
+@export var currentWave: int
 @export var padding: float = 50.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timer.start(spawnRate)
 	var viewport_size = get_viewport_rect().size
-	var spawn_pos = Vector2(viewport_size.x + padding, randf_range(0, viewport_size.y))
+	var spawn_pos = Vector2(viewport_size.x + padding, randf_range(0, viewport_size.y)) 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-
-func _on_timer_timeout() -> void:
-	for mob in mobsToSpawn:
+func _wave_Checklist() -> void:
+	if mob_types != null:
+		print("Starting Wave: ",currentWave,"!")
+		
+	else:
+		print("Null value in wave data!")
 		pass
+
 
 func _wave_ended():
 	print("stopped MobSpawner.")
 	queue_free()
+
+# Wave data contains the type of enemy and number.
+
+func _wave_reader(waveData: Array) -> Array:
+	var formatedWaveData = waveData
+	return formatedWaveData
